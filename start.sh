9bdca32f9d60bf951564f10e3919186cd8428a72
Daniel Evans <me@daredoes.work>
2022-02-10 23:22:37 -0800

    update start files

--- start.sh
+++ start.sh
@@ -9,13 +9,4 @@ fi
 
 python3 /start.py start
 
-#Define cleanup procedure
-cleanup() {
-    echo "Container stopped, performing cleanup..."
-    python3 /start.py stop
-}
-#Trap SIGTERM
-# trap 'cleanup' TERM
-
-# python3 /start.py stop & 
 /usr/bin/supervisord
